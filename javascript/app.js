40bf11eb5e1d5f15c01c31efa681d06b6ddaffb7
Eugene T <tyanbz@gmail.com>
2023-09-05 00:28:46 +0600

    start calculator

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+let num = 42;
+let firstName = 'Eugene';
+const isProgrammer = true;
+
+let $ = 'test';
+let $num = 42;
+let num$ = 42;
+let _ = 49;
+let _num = 12;
+let num_ = 12;
+let camelCase = 'style of code';
+
+// alert('Hello page!');
+console.log(firstName + ' ' + isProgrammer);
+console.log(42 + 10);
+
+const resultElement = document.getElementById('result');
+console.log(resultElement.textContent);
+
+resultElement.textContent = '15';
+
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+
+console.log(`Вы ввели ${input1.value} и ${input2.value}`)
